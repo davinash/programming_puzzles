a5b60b9193206e71d8c7625e7126435c1e538f8e
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-03 21:12:43 +0530

    IInd solution, quite a bit of improvement over previous solution 6 test cases passed out of 10

--- interview_street/string_similarity.cpp
+++ interview_street/string_similarity.cpp
@@ -3,56 +3,38 @@
 *     dongre.avinash@gmail.com          *
 * * * * * * * * * * * * * * * * * * * * */
 
-/* O(n-square) solution 
- * Logically corrected
- * but bot did not accept is 
+/* Simple straightforard solution
+ * Still O(n-Square), but no STL usage.
+ * Still not accepted by BOT.
  */
 
 #include <string>
-#include <algorithm>
 #include <iostream>
-#include <vector>
 
-using std::vector;
-using std::string;
-
-size_t longest_common_prefix( const vector<string>& inputs ) {
-    vector<string>::const_iterator itr = inputs.begin() ;
-    size_t prefix_count = itr->length() ;
-    string comp_str = *itr ;
-    for ( itr = inputs.begin() + 1; itr != inputs.end() ; itr++ ) {
-        std::pair<string::const_iterator , string::const_iterator> p = std::mismatch( comp_str.begin(), comp_str.end(), itr->begin()) ;
-        if (( p.first - comp_str.begin()) < (int)prefix_count ) 
-            prefix_count = p.first - comp_str.begin() ;
+inline void solve(const std::string& input) {
+    int len = input.length();
+    int count = 0;
+    int total = 0;
+    for( register int i = 1;i < len;++i) {
+        count=0;
+        for( register int j = i;j < len;++j) {
+            if(input[j - i] == input[j]) 
+                count++;
+            else
+                break;
+        }
+        total += count;
     }
-    return prefix_count;
+    std::cout << total + len << std::endl;;
 }
 
 int main ( int argc, char **argv) {
     int T;
     std::cin >> T;
-
-    std::vector<size_t> result;
-
     for ( int i = 0; i < T; i++) { 
         std::string str;
         std::cin >> str;
-        size_t sol = 0;
-        for ( string::iterator it = str.begin(); it != str.end(); ++it) {
-            vector<string> strings;
-            strings.push_back(std::string(it,str.end()));
-            strings.push_back(str);   
-            
-            sol += longest_common_prefix(strings);
-
-            strings.clear();
-            
-        }
-        result.push_back(sol);
+        solve ( str );
     }
-    for ( std::vector<size_t>::const_iterator it = result.begin(); it != result.end(); ++it) {
-        std::cout << *it << std::endl;
-    }
-    result.clear();
     return 0;
 }
