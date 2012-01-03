64a6d337b11f870fb27128aa4428d28c92383cbf
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-03 22:51:57 +0530

    Removed usage of STL vector.

--- interview_street/circle_summation.cpp
+++ interview_street/circle_summation.cpp
@@ -4,48 +4,42 @@
 * * * * * * * * * * * * * * * * * * * * */
 
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <iterator>
+#include <stdio.h>
 
-/* Slower version
- * Logically correct, but need to optimize further
- */
+/* 
+ * Use static memory, no STL vector.
+*/
 
 int main ( int argc, char **argv) {
     int T; // number of test cases
     std::cin >> T;
-
     for ( int i = 0 ; i < T; i++) {
-        std::vector<long long> childrens;
-
         int N; // number of childrens
         std::cin >> N;
-
         int M; // number of rounds
         std::cin >> M;
-
-        childrens.resize(N);
+        long long childrens[50];
         for ( int j = 0; j < N; j++ ) 
             std::cin >> childrens[j];
 
         int pos = 0;
         for ( int i = 0; i < N ; i++) {
-            std::vector< long long > childs = childrens;            
+            long long childs[50];
+            for ( int j = 0; j < N; j++ ) 
+                childs[j] = childrens[j];
             for ( int j = 0; j < M; j++) {
                 int left  = ( pos     == 0)  ? N - 1  : pos - 1;
                 int right = ( pos + 1 >= N ) ? 0      : pos + 1;
-
-                childs[pos++] += childs[left] + childs[right];
-
+                childs[pos] = (childs[pos] + childs[left] + childs[right])% 1000000007;
+                pos++;
                 pos   = ( pos     >= N ) ? 0      : pos;
             }
-            std::copy(childs.begin(),childs.end(),std::ostream_iterator<long long>(std::cout," "));
+            std::copy(childs ,childs + N ,std::ostream_iterator<long long>(std::cout," "));
             std::cout << std::endl;
-            childs.clear();
         }
         std::cout << std::endl;
-        childrens.clear();
     }
     return 0;
 }
