20c7d1b0b5d8ceb19ce36306239cbd3ecb844049
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-03 21:14:07 +0530

    Trying Suffix array approach.

--- interview_street/string_similarity.cpp
+++ interview_street/string_similarity.cpp
@@ -3,38 +3,56 @@
 *     dongre.avinash@gmail.com          *
 * * * * * * * * * * * * * * * * * * * * */
 
-/* Simple straightforard solution
- * Still O(n-Square), but no STL usage.
+/* suffix array approach.
  * Still not accepted by BOT.
  */
 
-#include <string>
+
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <string.h>
+#include <vector>
 
-inline void solve(const std::string& input) {
-    int len = input.length();
-    int count = 0;
-    int total = 0;
-    for( register int i = 1;i < len;++i) {
-        count=0;
-        for( register int j = i;j < len;++j) {
-            if(input[j - i] == input[j]) 
-                count++;
-            else
-                break;
-        }
-        total += count;
+std::string str;
+size_t len;
+
+struct Comp {
+    const char* str;
+    Comp(const char* str): str(str) {}
+    bool operator()(size_t s1, size_t s2) const {
+        return strcmp(str+s1, str+s2) < 0;
     }
-    std::cout << total + len << std::endl;;
-}
+};
 
-int main ( int argc, char **argv) {
-    int T;
+size_t lcp(size_t t) {
+    for (size_t i = 0; i < len - t; i++) {
+        if (str[i] != str[i + t]) 
+            return i;
+    }
+    return len - t;
+}
+int main(int argc, char **argv) {
+    size_t T;
     std::cin >> T;
-    for ( int i = 0; i < T; i++) { 
-        std::string str;
+    for ( size_t i = 0; i < T; i++) { 
         std::cin >> str;
-        solve ( str );
+        std::vector<size_t> SA;
+        len = str.size();
+        /* build Suffix array */
+        SA.resize(len + 1);
+        for (size_t i = 0; i < len + 1; ++i) { 
+            SA[i] = i; 
+        }
+        std::sort(SA.begin(), SA.end(), Comp(str.c_str()));
+
+        size_t total = 0;
+        for ( std::vector<size_t>::iterator it = SA.begin(); it != SA.end(); ++it) {
+            if (*it == 0 ) 
+                break;
+            total += lcp(*it);
+        }
+        std::cout << total + len << std::endl;
     }
     return 0;
 }
