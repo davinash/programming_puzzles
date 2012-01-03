802a5ba43e885daa753d6ce61ede63c3c1e53f68
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-03 21:10:38 +0530

    First solution, very slow O(n-square)

--- interview_street/string_similarity.cpp
+++ interview_street/string_similarity.cpp
@@ -1,76 +1,58 @@
+/* * * * * * * * * * * * * * * * * * * * * 
+*     Dedicated to the public domain    *
+*     dongre.avinash@gmail.com          *
+* * * * * * * * * * * * * * * * * * * * */
 
-/* dedicated to the public domain
- * dongre.avinash@gmail.com
+/* O(n-square) solution 
+ * Logically corrected
+ * but bot did not accept is 
  */
-inline bool leq(int a1, int a2,   int b1, int b2) { 
-    return(a1 < b1 || a1 == b1 && a2 <= b2); 
-}                                                   
-inline bool leq(int a1, int a2, int a3,   int b1, int b2, int b3) {
-    return(a1 < b1 || a1 == b1 && leq(a2,a3, b2,b3)); 
-}
 
-static void radixPass(int* a, int* b, int* r, int n, int K) 
-{ 
-    int* c = new int[K + 1];
-    for (int i = 0;  i <= K;  i++) c[i] = 0;
-    for (int i = 0;  i < n;  i++) c[r[a[i]]]++; 
-    for (int i = 0, sum = 0;  i <= K;  i++) { 
-        int t = c[i];  c[i] = sum;  sum += t;
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+size_t longest_common_prefix( const vector<string>& inputs ) {
+    vector<string>::const_iterator itr = inputs.begin() ;
+    size_t prefix_count = itr->length() ;
+    string comp_str = *itr ;
+    for ( itr = inputs.begin() + 1; itr != inputs.end() ; itr++ ) {
+        std::pair<string::const_iterator , string::const_iterator> p = std::mismatch( comp_str.begin(), comp_str.end(), itr->begin()) ;
+        if (( p.first - comp_str.begin()) < (int)prefix_count ) 
+            prefix_count = p.first - comp_str.begin() ;
     }
-    for (int i = 0;  i < n;  i++) b[c[r[a[i]]]++] = a[i];      
-    delete [] c;
+    return prefix_count;
 }
-void suffixArray(int* s, int* SA, int n, int K) {
-    int n0=(n+2)/3, n1=(n+1)/3, n2=n/3, n02=n0+n2; 
-    int* s12  = new int[n02 + 3];  s12[n02]= s12[n02+1]= s12[n02+2]=0; 
-    int* SA12 = new int[n02 + 3]; SA12[n02]=SA12[n02+1]=SA12[n02+2]=0;
-    int* s0   = new int[n0];
-    int* SA0  = new int[n0];
-    for (int i=0, j=0;  i < n+(n0-n1);  i++) if (i%3 != 0) s12[j++] = i;
 
+int main ( int argc, char **argv) {
+    int T;
+    std::cin >> T;
 
-    radixPass(s12 , SA12, s+2, n02, K);
-    radixPass(SA12, s12 , s+1, n02, K);  
-    radixPass(s12 , SA12, s  , n02, K);
+    std::vector<size_t> result;
 
+    for ( int i = 0; i < T; i++) { 
+        std::string str;
+        std::cin >> str;
+        size_t sol = 0;
+        for ( string::iterator it = str.begin(); it != str.end(); ++it) {
+            vector<string> strings;
+            strings.push_back(std::string(it,str.end()));
+            strings.push_back(str);   
+            
+            sol += longest_common_prefix(strings);
 
-    int name = 0, c0 = -1, c1 = -1, c2 = -1;
-    for (int i = 0;  i < n02;  i++) {
-        if (s[SA12[i]] != c0 || s[SA12[i]+1] != c1 || s[SA12[i]+2] != c2) { 
-            name++;  c0 = s[SA12[i]];  c1 = s[SA12[i]+1];  c2 = s[SA12[i]+2];
+            strings.clear();
+            
         }
-        if (SA12[i] % 3 == 1) { s12[SA12[i]/3]      = name; } 
-        else                  { s12[SA12[i]/3 + n0] = name; } 
+        result.push_back(sol);
     }
-
-    if (name < n02) {
-        suffixArray(s12, SA12, n02, name);
-
-        for (int i = 0;  i < n02;  i++) s12[SA12[i]] = i + 1;
-    } else 
-        for (int i = 0;  i < n02;  i++) SA12[s12[i] - 1] = i; 
-
-    for (int i=0, j=0;  i < n02;  i++) if (SA12[i] < n0) s0[j++] = 3*SA12[i];
-    radixPass(s0, SA0, s, n0, K);
-
-    for (int p=0,  t=n0-n1,  k=0;  k < n;  k++) {
-#define GetI() (SA12[t] < n0 ? SA12[t] * 3 + 1 : (SA12[t] - n0) * 3 + 2)
-        int i = GetI(); 
-        int j = SA0[p]; 
-        if (SA12[t] < n0 ? 
-            leq(s[i],       s12[SA12[t] + n0], s[j],       s12[j/3]) :
-        leq(s[i],s[i+1],s12[SA12[t]-n0+1], s[j],s[j+1],s12[j/3+n0]))
-        { 
-            SA[k] = i;  t++;
-            if (t == n02) {
-                for (k++;  p < n0;  p++, k++) SA[k] = SA0[p];
-            }
-        } else { 
-            SA[k] = j;  p++; 
-            if (p == n0)  {
-                for (k++;  t < n02;  t++, k++) SA[k] = GetI(); 
-            }
-        }  
-    } 
-    delete [] s12; delete [] SA12; delete [] SA0; delete [] s0; 
+    for ( std::vector<size_t>::const_iterator it = result.begin(); it != result.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+    result.clear();
+    return 0;
 }
