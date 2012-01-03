e0ac22abebb8d5fb0126708456479947a60634d0
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-03 19:10:38 +0530

    First implementation, it works for smaller inputs, still giving error that Time Limit Exceeded So I am calling this as a Slow version.

--- interview_street/circle_summation.cpp
+++ interview_street/circle_summation.cpp
@@ -1,19 +1,12 @@
-/* dedicated to the public domain
-* dongre.avinash@gmail.com
-*/
+/* * * * * * * * * * * * * * * * * * * * * 
+*     Dedicated to the public domain    *
+*     dongre.avinash@gmail.com          *
+* * * * * * * * * * * * * * * * * * * * */
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-struct cd_list {
-    long long data;
-    struct cd_list *next;
-    struct cd_list *prev;
-};
-
-cd_list *root    = (cd_list*)0;
-cd_list *current = (cd_list*)0;
-cd_list *temp    = (cd_list*)0;
+#include <iterator>
 
 int main ( int argc, char **argv) {
     int T; // number of test cases
@@ -21,56 +14,34 @@ int main ( int argc, char **argv) {
 
     for ( int i = 0 ; i < T; i++) {
         std::vector<long long> childrens;
+
         int N; // number of childrens
         std::cin >> N;
+
+        int M; // number of rounds
+        std::cin >> M;
+
         childrens.resize(N);
-        int I; // starting position
-        std::cin >> I;
-        for ( int j = 0; j < N; j++) {
-            if ( !root ) {
-                root = new cd_list;
-                std::cin >> root->data;
-                root->next = (cd_list*)0;
-                root->prev = (cd_list*)0;
-                current = root;
-            } else {
-                temp = new cd_list;
-                std::cin >> temp->data;
-                temp->next = (cd_list*)0;
-                temp->prev = current;
-                current->next = temp;
-                current = temp;
-            }
-        }
-        // complete the circular link.
-        root->prev = temp;
-        temp->next = root;
+        for ( int j = 0; j < N; j++ ) 
+            std::cin >> childrens[j];
 
-        cd_list *moving = root;
-        for ( int i = 0; i < N; i++) {
-            for ( int j = 0; j < N; j++) {
-                if ( j + 1 == I ) {
-                    std::cout << moving->prev->data + moving->next->data + moving->data << " ";
-                    moving = moving->next;
-                    continue;
-                }
-                std::cout << moving->data << " ";
-                moving = moving->next;
-            }
-            std::cout << std::endl; // done with first round.
-            I = I + 1; // increment to next child
-            moving = root; // reset the start position
-        }
-        // delete the double circular linked list
-        moving = root->next;
-        while ( moving != root ) {
-            cd_list *temp = moving;
-            moving = moving->next;
-            delete temp;
-        }
-        delete root;
-        root = ( cd_list *) 0;
+        int pos = 0;
+        for ( int i = 0; i < N ; i++) {
+            std::vector< long long > childs = childrens;            
+            for ( int j = 0; j < M; j++) {
+                int left  = ( pos     == 0)  ? N - 1  : pos - 1;
+                int right = ( pos + 1 >= N ) ? 0      : pos + 1;
 
+                childs[pos++] += childs[left] + childs[right];
+
+                pos   = ( pos     >= N ) ? 0      : pos;
+            }
+            std::copy(childs.begin(),childs.end(),std::ostream_iterator<long long>(std::cout," "));
+            std::cout << std::endl;
+            childs.clear();
+        }
+        std::cout << std::endl;
+        childrens.clear();
     }
     return 0;
 }
