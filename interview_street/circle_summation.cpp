98d6b480d4716a2fe02f0da2b9ed0a4b1baf59dc
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-03 05:33:16 +0530

    Initial version, trying to solve using the 1-d array

--- interview_street/circle_summation.cpp
+++ interview_street/circle_summation.cpp
@@ -1,13 +1,14 @@
 /* dedicated to the public domain
- * dongre.avinash@gmail.com
- */
+* dongre.avinash@gmail.com
+*/
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main ( int argc, char **argv) {
     int T; // number of test cases
     std::cin >> T;
-    
+
     for ( int i = 0 ; i < T; i++) {
         std::vector<long long> childrens;
         int N; // number of childrens
@@ -18,6 +19,26 @@ int main ( int argc, char **argv) {
         for ( int j = 0; j < N; j++) {
             std::cin >> childrens[j];
         }
+        for ( int i = 0; i < N; i++) {
+            for ( int j = 0; j < N; j++) {
+                if ( j + 1 == I ) {
+                    if ( j == i ) {
+                        std::cout << childrens[j] + childrens[N - 1] + childrens[1] << " ";
+                    }
+                    else if ( j + 1 == N ) {
+                        std::cout << childrens[j] + childrens[j - 1] + childrens[0] << " ";
+                    }
+                } else {
+                    std::cout << childrens[j] << " ";
+                }
+            }
+            if ( I == N )
+                I = 0;
+            else 
+                I++;
+
+            std::cout << std::endl;
+        }        
     }
     return 0;
 }
