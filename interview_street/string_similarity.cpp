7343dc74910c3a64090b1df434548d71e161aa00
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-05 15:20:00 +0530

    Using z-Algorithm to solve and make interview-street happy.

--- interview_street/string_similarity.cpp
+++ interview_street/string_similarity.cpp
@@ -1,34 +1,36 @@
 /* * * * * * * * * * * * * * * * * * * * * 
-*     Dedicated to the public domain    *
-*     dongre.avinash@gmail.com          *
-* * * * * * * * * * * * * * * * * * * * */
+*     Dedicated to the public domain     *
+*     dongre.avinash@gmail.com           *
+* * * * * * * * * * * * * * * * * * * * **/
 
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <numeric>
 
 int main ( int argc, char **argv) {
     size_t T;
     std::cin >> T;
     char input[100000];
-    for ( size_t i = 0; i < T; i++) {
+    int Z[100000];
+    for ( register size_t i = 0; i < T; ++i) {
         std::cin >> input;
-        size_t len    = strlen(input);
-        char *left    = input;
-        char *right   = input + len - 1;
-        int sol       = 0;
-        int end_count = 1;
-        while ( left < right ) {
-            if ( *right != '\0') {
-                if ( *left == *right ) {
-                    sol++;
-                    left++;right++;
-                    continue;
-                }
+        int N = strlen ( input);
+
+        Z[0] = N;
+        int a = 0, b = 0;
+        for (int i = 1; i < N; ++i) {
+            int k = i < b ? std::min(b - i, Z[i - a]) : 0;
+            while (i + k < N && input[i + k] == input[k]) 
+                ++k;
+            Z[i] = k;
+            if (i + k > b) { 
+                a = i; b = i + k; 
             }
-            end_count++;
-            left = input;
-            right = input + len - end_count;
         }
-        std::cout << sol + len << std::endl;
+        int sol = 0;
+        for ( int i = 0; i < N; i++) {
+            sol += Z[i];
+        }
+        std::cout << sol << std::endl;
     }
 }
