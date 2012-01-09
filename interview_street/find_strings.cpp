6411c56ac73c28ea1537ff22318ec6dd36da554d
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-09 18:20:55 +0530

    another try.

--- interview_street/find_strings.cpp
+++ interview_street/find_strings.cpp
@@ -2,12 +2,8 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <functional>
-
-/* Simple O(n2) Solution */
 
 std::vector<std::string> S;
-
 void solve() {
     size_t Qi = 0;
     std::cin >> Qi;
@@ -19,36 +15,30 @@ void solve() {
     }
 }
 int main ( int argc, char **argv ) {
-    int n; // number of strings
+    int n;
     std::cin >> n;
     for ( int i = 0; i < n ; i++){
-        // Read the string
         std::string str;
-        std::cin >> str;
-        std::vector<std::string> Stemp;
-        size_t length = str.size();
-        for( size_t i = 0 ; i < length ; i++ ) {
-            for( size_t j = 1 ; j <= length - i ; j++ ) {
-                Stemp.push_back(str.substr(i, i + j));
+        std::cin >> str;       
+
+        for (unsigned int mask = 1; mask < (1 << str.size()); ++mask) {
+            std::string::const_iterator it = str.begin();
+            std::string sub_str;
+            for (unsigned int m = mask; m; (m>>=1), ++it) {      
+                if (m & 1) {
+                    sub_str.push_back(*it);
+                }
             }
+            S.push_back(sub_str);
         }
-        std::sort(Stemp.begin(), Stemp.end());
-        Stemp.erase(std::unique(Stemp.begin(), Stemp.end()), Stemp.end());
-
-        std::vector<std::string> Stemp_result;
-        Stemp_result.resize (Stemp.size() + S.size());
-
-        std::set_union(Stemp.begin(), Stemp.end(), S.begin(), S.end(), Stemp_result.begin());
-        S = Stemp_result;
-        
     }
-    std::vector<std::string>::iterator it = std::remove_if(S.begin(), S.end(),std::mem_fun_ref(&std::string::empty));
-    S.erase(it, S.end());
-
-    int q; // number of queries
+    std::sort(S.begin(), S.end());
+    S.erase(std::unique(S.begin(), S.end()), S.end());
+    int q;
     std::cin >> q;
     for ( int i = 0; i < q; i++) {
         solve();
     }
+
     return 0;
 }
