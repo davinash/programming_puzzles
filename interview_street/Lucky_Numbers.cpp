f7a50debbf20122b7ee4551b6ab68b69b2b47901
Avinash Dongre <dongre.avinash@gmail.com>
2012-01-23 15:55:39 +0530

    attempt 2 1. Do not check square function if the sum is not prime 2. there is no need to perform iteration over the number twice 3. Do not call mod operation twice.

--- interview_street/Lucky_Numbers.cpp
+++ interview_street/Lucky_Numbers.cpp
@@ -2,29 +2,22 @@
 #include <string>
 #include <numeric>
 /* trying with brute force approach */
-long long sum_of_digits(long long input) {
-	long long total = 0;
-	while (input != 0) {
-		total += input % 10;
-		input /= 10;
-	}
-	return total;
-}
-long long sum_of_square_digits(long long input) {
-	long long total = 0;
-	while (input != 0) {
-		total += (input % 10)* (input % 10);
-		input /= 10;
-	}
-	return total;
+void sum_of_digits_and_square(long long input, long long& sum, long long& sum_square) {
+	sum = sum_square = 0L;
+	while (input != 0L) {
+		long long digit = input % 10L;
+		sum        += digit;
+		sum_square += digit * digit;
+		input /= 10L;
+	}	
 }
 bool isPrime(long long x) {
-        long long max = static_cast <long long> (x);
-        if (x <= 0 || x == 1 || (x % 2 == 0 && x != 2)) return false;
-        for (long long i = 3; i < max; i += 2)
-            if (x % i == 0)
-                return false;
-        return true;
+	long long max = static_cast <long long> (x);
+	if (x <= 0 || x == 1 || (x % 2 == 0 && x != 2)) return false;
+	for (long long i = 3; i < max; i += 2)
+		if (x % i == 0)
+			return false;
+	return true;
 }
 int main ( int argc, char** argv) {
 	int T;
@@ -36,10 +29,13 @@ int main ( int argc, char** argv) {
 		long long B;
 		std::cin >> A >> B;
 		for ( long long i = A; i <= B; i++) {
-			long long sum        = sum_of_digits( i );
-			long long sum_square = sum_of_square_digits( i );
-			if ( isPrime(sum) && isPrime(sum_square)){
-				result++;
+			long long sum        = 0;
+			long long sum_square = 0;
+			sum_of_digits_and_square(i,sum,sum_square);
+			if ( isPrime(sum)) {
+				if ( isPrime(sum_square)){
+					result++;
+				}
 			}
 		}
 		std::cout << result << std::endl;
